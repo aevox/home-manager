25f79e6bafeb080daf8d780ccc56aa03e56650ad
Marc Fouché <aevox@users.noreply.github.com>
2023-05-23 10:55:22 +0200

    nvim: add airline tabline

--- nvim/nvim.nix
+++ nvim/nvim.nix
@@ -123,6 +123,11 @@ in
         plugin = vim-airline;
         config = ''
           let g:airline_theme = 'onedark'
+          let g:airline#extensions#tabline#enabled = 1
+          let g:airline#extensions#tabline#left_sep = ""
+          let g:airline#extensions#tabline#left_alt_sep = ""
+          let g:airline#extensions#tabline#right_sep = ""
+          let g:airline#extensions#tabline#right_alt_sep = ""
         '';
       }
     ];
