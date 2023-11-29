06cbf385a5ce72234c40dd4cda48062454928798
Marc Fouché <aevox@users.noreply.github.com>
2023-11-29 14:51:09 +0100

    pop-shell: Use version compatible with gnome42

--- gnome/pop-shell.nix
+++ gnome/pop-shell.nix
@@ -1,9 +1,18 @@
-{ config, pkgs, lib, ... }: {
+{ config, pkgs, lib, ... }:
+let
+    nixpkgs-2023-03-31 = import (builtins.fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/1b7a6a6e57661d7d4e0775658930059b77ce94a4.tar.gz";
+    }) {};
+
+    pop-shell-2023-03-31 = nixpkgs-2023-03-31.gnomeExtensions.pop-shell;
+in
+{
   home.packages = with pkgs; [
     pop-launcher
     pop-gtk-theme
     pop-icon-theme
     gnomeExtensions.pop-launcher-super-key
-    gnomeExtensions.pop-shell
+
+    pop-shell-2023-03-31
   ];
 }
